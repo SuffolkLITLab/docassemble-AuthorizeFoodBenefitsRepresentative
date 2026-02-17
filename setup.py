620f615a7b5f55ce12539de476736948cae8d2ba
Bryce Willey <bryce.willey@suffolk.edu>
2026-02-17 13:29:11 -0500

    Add ALKiln on GitHub in Actions

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(name='docassemble.AuthorizeFoodBenefitsRepresentative',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
       packages=find_namespace_packages(),
-      install_requires=[],
+      install_requires=['docassemble.AssemblyLine', 'docassemble.MassAccess'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/AuthorizeFoodBenefitsRepresentative/', package='docassemble.AuthorizeFoodBenefitsRepresentative'),
      )
